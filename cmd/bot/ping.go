ff2928db8f0691cd7ab97e91165e4ea5fc6a3070
ChinGH <86087456+chinghytb@users.noreply.github.com>
2021-12-12 03:48:47 +0800

    Create ping.go

---
+++ cmd/bot/ping.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	token := "Bot Token Here"
+
+	s, err := discordgo.New("Bot " + token)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = s.Open(); err != nil {
+		panic(err)
+	}
+
+	s.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	s.Identify.Intents = discordgo.IntentsGuildMessages
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit...")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	s.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	// If the message is "ping" reply with "Pong!"
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if m.Content == "pong" {
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+}
